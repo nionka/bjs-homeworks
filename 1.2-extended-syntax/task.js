a8964e824f83bf9ecf46a1e475fbf2f644a3f8be
Olga Runova <ockaaa@gmail.com>
2020-06-12 00:50:13 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,52 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+
+    let x = [];
+    let d = b ** 2 - 4 * a * c;
+
+    if (d === 0) {
+        x[0] = -b / (2 * a);
+    } else if (d > 0) {
+        x[0] = (-b + Math.sqrt(d)) / (2 * a);
+        x[1] = (-b - Math.sqrt(d)) / (2 * a);
+    } else {
+        x = [];
+    }
+
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark = 0;
+    let sum = 0;
+
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        console.log("Оценок слишком много, беру первые 5");
+        marks = marks.slice(0, 5);
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    averageMark = sum / marks.length;
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let userYear = dateOfBirthday.getFullYear();
+    let thisYear = new Date().getFullYear();
+    let ageGap = thisYear - userYear;
+    let result = "";
+
+
+    if (ageGap > 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+    return result;
 }
