3c5fa5d7c00cd454cc567f281f47fdf54a164f11
Olga Runova <ockaaa@gmail.com>
2020-06-30 12:44:54 +0300

    fix added

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -30,16 +30,17 @@ function getAverageMark(marks) {
 
 function checkBirthday(birthday) {
     let now = +Date.now();
-    birthday = +new Date(birthday);
-    let diff = now - birthday;
-    let mlsInYear = 1000 * 60 * 60 * 24 * 365.25;
+    const birthdayDate = +new Date(birthday);
+    let diff = now - birthdayDate;
+    const mlsInYear = 1000 * 60 * 60 * 24 * 365.25;
     let age = diff / mlsInYear;
 
-    if (age > 18) {
-        return true
-    } else {
-        return false
-    }
+    // if (age > 18) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+    return age >= 18
 
     // return verdict
 }
